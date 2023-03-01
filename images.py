0b36e02a669369e783fbafe846c4ce08fe9ce8ff
Peter Bloem <git@peterbloem.nl>
2023-03-01 16:53:44 +0100

    Add mastodon posting

--- images.py
+++ images.py
@@ -3,7 +3,13 @@ from PIL import Image, ImageDraw, ImageFont
 from argparse import ArgumentParser
 import random, textwrap
 
-def go(arg):
+import requests, sys, os, time, yaml
+from requests_oauthlib import OAuth1
+
+import mastodon
+from mastodon import Mastodon
+
+def go(arg, twitter=True, mastodon=True):
 
     # load the strategies
     with open('strategies.txt', 'r') as file:
@@ -32,125 +38,145 @@ def go(arg):
     print('Image created')
 
     """
-    Post the image final.png to the tony ants twitter account.
+    Post the image final.png to the obliquademia twitter account.
     
     """
 
-    import requests, sys, os, time, yaml
-    from requests_oauthlib import OAuth1
-
-    def check(r):
-        if r.status_code < 200 or r.status_code > 299:
-            print(r.status_code, r.reason)
-            print(r.text)
-            sys.exit()
+    if twitter:
 
 
-    # Creds for @obliquademia
-    if os.path.exists('auth.yaml'):
-        creds = yaml.load(open('auth.yaml', 'r'))
-    else:
-        creds = {
-            'app_key' : os.environ['APPKEY'],
-            'app_secret' : os.environ['APPSECRET'],
-            'user_key' : os.environ['USERKEY'],
-            'user_secret' : os.environ['USERSECRET']
-        }
+        def check(r):
+            if r.status_code < 200 or r.status_code > 299:
+                print(r.status_code, r.reason)
+                print(r.text)
+                sys.exit()
 
-    auth = OAuth1(
-        creds['app_key'], # app key
-        creds['app_secret'], # app secret
-        creds['user_key'], # user key
-        creds['user_secret'] # user secret
-    )
 
-    # Upload the image (chunked upload)
+        # Creds for @obliquademia
+        if os.path.exists('auth.twitter.yaml'):
+            creds = yaml.safe_load(open('auth.twitter.yaml', 'r'))
+        else:
+            creds = {
+                'app_key' : os.environ['APPKEY'],
+                'app_secret' : os.environ['APPSECRET'],
+                'user_key' : os.environ['USERKEY'],
+                'user_secret' : os.environ['USERSECRET']
+            }
 
-    URL = 'https://upload.twitter.com/1.1/media/upload.json'
+        auth = OAuth1(
+            creds['app_key'], # app key
+            creds['app_secret'], # app secret
+            creds['user_key'], # user key
+            creds['user_secret'] # user secret
+        )
 
-    filename = 'final.png'
-    mime = 'image/png'
+        # Upload the image (chunked upload)
 
-    file = open(filename, 'rb')
-    total_bytes = os.path.getsize(filename)
+        URL = 'https://upload.twitter.com/1.1/media/upload.json'
 
-    ## INIT
+        filename = 'final.png'
+        mime = 'image/png'
 
-    data = {
-          'command': 'INIT',
-          'media_type': mime,
-          'total_bytes': total_bytes,
-          'media_category': 'tweet_image'
-        }
+        file = open(filename, 'rb')
+        total_bytes = os.path.getsize(filename)
 
-    r = requests.post(URL, data=data, auth=auth)
-
-    check(r)
-
-    if 'media_id' in r.json():
-        media_id = r.json()['media_id']
-    else:
-        raise Exception('Media id not found. Response ', r.json())
-
-    # APPEND
-
-    bytes_sent = 0
-    segment_id = 0
-    while bytes_sent < total_bytes:
-        print('appending')
+        ## INIT
 
         data = {
-            'command': 'APPEND',
-            'media_id': media_id,
-            'segment_index': segment_id
-        }
+              'command': 'INIT',
+              'media_type': mime,
+              'total_bytes': total_bytes,
+              'media_category': 'tweet_image'
+            }
 
-        files = {
-            'media': file.read(4 * 1024 * 1024)
-        }
+        r = requests.post(URL, data=data, auth=auth)
 
-        r = requests.post(url=URL, data=data, files=files, auth=auth)
         check(r)
 
-        segment_id += 1
-        bytes_sent = file.tell()
+        if 'media_id' in r.json():
+            media_id = r.json()['media_id']
+        else:
+            raise Exception('Media id not found. Response ', r.json())
 
-        print(f'{bytes_sent/1024:.5} of {total_bytes/1024:.5} kbytes uploaded')
+        # APPEND
 
-    # FNALIZE
+        bytes_sent = 0
+        segment_id = 0
+        while bytes_sent < total_bytes:
+            print('appending')
 
-    data = {
-      'command': 'FINALIZE',
-      'media_id': media_id
-    }
+            data = {
+                'command': 'APPEND',
+                'media_id': media_id,
+                'segment_index': segment_id
+            }
 
-    print('finalizing')
-    r = requests.post(url=URL, data=data, auth=auth)
-    check(r)
-    print('FINALIZE', r.json())
+            files = {
+                'media': file.read(4 * 1024 * 1024)
+            }
 
-    # Add alt text to image
+            r = requests.post(url=URL, data=data, files=files, auth=auth)
+            check(r)
 
-    URL = 'https://upload.twitter.com/1.1/media/metadata/create.json'
-    data = {
-        'media_id': media_id,
-        'alt_text': {
-            'text': strategy_orig
+            segment_id += 1
+            bytes_sent = file.tell()
+
+            print(f'{bytes_sent/1024:.5} of {total_bytes/1024:.5} kbytes uploaded')
+
+        # FNALIZE
+
+        data = {
+          'command': 'FINALIZE',
+          'media_id': media_id
         }
-    }
 
-    r = requests.post(URL, json=data, auth=auth)
-    check(r)
+        print('finalizing')
+        r = requests.post(url=URL, data=data, auth=auth)
+        check(r)
+        print('FINALIZE', r.json())
 
-    # Post the tweet
-    URL = 'https://api.twitter.com/1.1/statuses/update.json'
-    data = {'status' : f'', 'media_ids' : media_id}
+        # Add alt text to image
 
-    r = requests.post(URL, data=data, auth=auth)
-    print(r.status_code, r.reason)
-    print(r.json())
+        URL = 'https://upload.twitter.com/1.1/media/metadata/create.json'
+        data = {
+            'media_id': media_id,
+            'alt_text': {
+                'text': strategy_orig
+            }
+        }
 
+        r = requests.post(URL, json=data, auth=auth)
+        check(r)
 
+        # Post the tweet
+        URL = 'https://api.twitter.com/1.1/statuses/update.json'
+        data = {'status' : f'', 'media_ids' : media_id}
+
+        r = requests.post(URL, data=data, auth=auth)
+        print(r.status_code, r.reason)
+        print(r.json())
+
+    """
+    Post the image final.png to the obliquademia mastodon account.
+
+    """
+
+    if mastodon:
+
+        # Creds for @obliquademia
+        if os.path.exists('auth.mastodon.yaml'):
+            creds = yaml.safe_load(open('auth.mastodon.yaml', 'r'))
+        else:
+            creds = {
+                'maccess_token': os.environ['MACCESS_TOKEN']
+            }
+
+        m = Mastodon(access_token=creds["maccess_token"], api_base_url="https://botsin.space")
+
+        metadata = m.media_post('final.png', mime_type='image/png', description=strategy)
+        # -- response format: https://mastodonpy.readthedocs.io/en/stable/#media-dicts
+
+        m.status_post("", media_ids=metadata['id'])
 
 if __name__ == "__main__":
 
